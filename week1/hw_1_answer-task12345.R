93ff3f5d48a526c8029d5edb29dae766b6172daf
Eddie Yen <rgmmmt4r@gmail.com>
2018-03-08 01:30:02 +0800

    week1-task12345

--- week1/hw_1_answer-task12345.R
+++ week1/hw_1_answer-task12345.R
@@ -57,14 +57,10 @@ for (x in c(26:50)) {
 ########################################################### Task 4
 
 # 請寫一段程式碼，能判斷輸入之西元年分 year 是否為閏年
-year<- function(){ 
-  n <- readline(prompt="Enter an integer: ")
-  return(as.integer(n))
-}
-k<<-year()
-#閏年的定義為4的倍數但不是100的倍數，或是400的倍數
-
-
+# 閏年的定義為4的倍數但不是100的倍數，或是400的倍數
+{
+k <-readline(prompt="Enter an integer: ")
+k <-as.integer(k)
 if (k %% 400 == 0){
   print("是閏年")
 }else if(k %% 4 ==0){
@@ -74,7 +70,7 @@ if (k %% 400 == 0){
     print("是閏年")
   }
 }
-
+}
 
 ########################################################### Task 5
 
@@ -83,8 +79,143 @@ if (k %% 400 == 0){
 # 2. 玩家可重覆猜電腦所產生的數字，並提示猜測的結果(EX:1A2B)
 # 3. 一旦猜對，系統可自動計算玩家猜測的次數
 
+# 隨機四位數
+{
+x<- sample(0:9,size=1)
+y<- sample(0:9,size=1)
+z<- sample(0:9,size=1)
+w<- sample(0:9,size=1)
+print(paste("隨機四位數：",x,y,z,w))
+# 計數器
+t<-0
 
+# 輸入數字的程式
+repeat{
+  
+k1 <-readline(prompt="請輸入千位: ")
+k1 <-as.integer(k1)
+k2 <-readline(prompt="請輸入百位: ")
+k2 <-as.integer(k2)
+k3 <-readline(prompt="請輸入十位: ")
+k3 <-as.integer(k3)
+k4 <-readline(prompt="請輸入個位: ")
+k4 <-as.integer(k4)
+#幾Ａ幾Ｂ
+a<-0
+b<-0
 
+t<-t+1
+
+# 比較的部分
+
+if (x==k1){
+  a<-a+1
+  if(k1==y){ #k1已經和x相等，若k1和y位置的數值相等，不可計入b，故在此扣掉
+    b<-b-1
+  }
+  if(k1==z){
+    b<-b-1
+  }
+  if(k1==w){
+    b<-b-1
+  }
+}else
+{
+  if(x==k2){
+    b<-b+1
+  }else if(x==k3)
+  {
+    b<-b+1
+  }else if(x==k4)
+  {
+    b<-b+1
+  }
+}
+if (y==k2)
+  {
+  a<-a+1
+  if(k2==x){
+    b<-b-1
+  }
+  if(k2==z){
+    b<-b-1
+  }
+  if(k2==w){
+    b<-b-1
+  }
+}else
+{
+  if(y==k1){
+    b<-b+1
+  }
+  else if(y==k3){
+    b<-b+1
+  }
+  else if(y==k4){
+    b<-b+1
+  }
+}
+if (z==k3)
+  {
+  if(k3==x){
+    b<-b-1
+  }
+  if(k3==y){
+    b<-b-1
+  }
+  if(k3==w){
+    b<-b-1
+  }
+  a<-a+1
+}else
+{
+  if(z==k1){
+    b<-b+1
+  }
+  else if(z==k2){
+    b<-b+1
+  }
+  else if(z==k4){
+    b<-b+1
+  }
+}
+if (w==k4)
+  {
+  a<-a+1
+  if(k4==x){
+    b<-b-1
+  }
+  if(k4==y){
+    b<-b-1
+  }
+  if(k4==z){
+    b<-b-1
+  }
+}else
+{
+  if(w==k1){
+    b<-b+1
+  }
+  else if(w==k2){
+    b<-b+1
+  }
+  else if(w==k3){
+    b<-b+1
+  }
+}
+print(paste(a,"A",b,"B"))
+if(x==k1){
+  if(y==k2){
+    if(z==k3){
+      if(w==k4){
+        print(paste('第',t,"次成功，恭喜你"))
+        break
+      }
+    }
+  }
+}
+}
+}
 
 
 
